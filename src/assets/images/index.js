5abe25b99e5ea61e8988f15a923cf2f14e6c8802
RohanSinghTomar <rohansingh.tomar@thecareerlabs.com>
2022-12-23 00:19:54 +0530

    Revert "icon changed"
    This reverts commit db521eb8c5cdff9d880c2f9eca7f4c03911005af.

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,2 +1,1 @@
 export {default as coin} from './coin.png';
-export {default as splash} from './splash.png';
--- src/screens/splash.js
+++ src/screens/splash.js
@@ -2,7 +2,6 @@ import {Image, StyleSheet, Text, View} from 'react-native';
 import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {COLORS} from '../assets/colors';
-import {splash} from '../assets/images/index';
 
 const SplashScreen = ({navigation}) => {
   const timeoutHelper = action => {
@@ -21,7 +20,13 @@ const SplashScreen = ({navigation}) => {
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <View style={styles.flex}>
-        <Image source={splash} resizeMode={'contain'} style={styles.image} />
+        <Image
+          source={{
+            uri: 'https://leaguex.com/_next/image?url=%2Fimg%2Flogos%2Flx_logo.png&w=3840&q=75',
+          }}
+          resizeMode={'contain'}
+          style={styles.image}
+        />
         <Text style={styles.upperTitle}>Pick Best Team</Text>
         <Text style={styles.lowerTitle}>WIN REAL MONEY</Text>
       </View>
